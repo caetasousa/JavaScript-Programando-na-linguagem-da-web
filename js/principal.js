47939fc5808a5c51e4ff919f6970cb9fc2116a86
eduardo <caetasousa@gmail.com>
2021-08-06 22:27:49 -0300

    Variaveis e operadores

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,35 @@
+// git checkout -b NOMEDABRANCH
+// git commit -m 'Comentário do commit da minha nova branch'
+// git push --set-upstream origin NOMEDABRANCH
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutri";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
